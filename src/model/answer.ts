81377ad811ee660d2f24aaff64869b9fbd2fa0df
Cedric Reichenbach <cedric.reichenbach@gmail.com>
2018-02-12 12:03:12 +0100

    Implement slides for question answering

---
+++ src/model/answer.ts
@@ -0,0 +1,6 @@
+export class Answer {
+  constructor(
+    public content: string,
+    public questionId: number
+  ) { }
+}
--- src/pages/questions/questions.ts
+++ src/pages/questions/questions.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Question } from '../../model/question';
+import { Answer } from '../../model/answer';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { QuestionsProvider } from '../../providers/questions/questions';
 
@@ -11,11 +12,23 @@ export class QuestionsPage implements OnInit {
 
   questions: Question[];
 
-  constructor(private questionsProvider: QuestionsProvider) {
+  answers: Answer[];
 
+  constructor(private questionsProvider: QuestionsProvider) {
   }
 
   ngOnInit(): void {
-    this.questionsProvider.listAll().subscribe(questions => this.questions = questions);
+    this.questionsProvider.listAll().subscribe(questions => {
+      this.questions = questions;
+      this.answers = questions.map(q => new Answer(null, q.id));
+    });
+  }
+
+  everthingAnswered(): boolean {
+    return this.answers && this.answers.every(a => !!a.content);
+  }
+
+  submitAnswers(): void {
+    // TODO: Send to server
   }
 }
