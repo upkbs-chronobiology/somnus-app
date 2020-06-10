d91c13a1210172a518665246fa40381b14df9a31
Cedric Reichenbach <cedric.reichenbach@gmail.com>
2020-06-10 12:17:31 +0200

    Default to 'prod' if ionic env variable is not set, e.g. during Azure web app build

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -3,7 +3,7 @@ var config = require('@ionic/app-scripts/config/webpack.config.js');
 
 module.exports = function () {
     config[process.env.IONIC_ENV].resolve.alias = {
-        "@environment": path.resolve(__dirname + '/../../src/config/config.' + process.env.IONIC_ENV + '.ts'),
+        "@environment": path.resolve(__dirname + '/../../src/config/config.' + (process.env.IONIC_ENV || 'prod') + '.ts'),
     };
 
     // by default mapped to IONIC_SOURCE_MAP_TYPE
